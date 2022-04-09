842302a1fcc5b688e3e460b201556fb9b1df74ce
pablo <estornut@gmail.com>
2022-04-10 01:55:53 +0200

    log hasHazy

--- server/rooms/voxters-service.ts
+++ server/rooms/voxters-service.ts
@@ -17,6 +17,7 @@ export const getUserNyanCats = async ({hasConnectedWeb3, publicKey}) => {
         const nyanIds = await getIds(publicKey, nyanContract, nyanCatIds)
         const hasNyanAoki = (await getIds(publicKey, aokiContract, [1])).length;
         const hasHazy = getBalance(publicKey, hazyContract);
+        console.log("hasHazy",hasHazy);
         const result = [...nyanIds];
         if(hasNyanAoki){
             result.push(10058);
